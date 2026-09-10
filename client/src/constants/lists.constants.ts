b2f8113951ddb1ff71e541dee2c3099c63a9d5c2
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-09-11 01:12:14 +0700

    chore(client): update debounce delay in lists constants

--- client/src/constants/lists.constants.ts
+++ client/src/constants/lists.constants.ts
@@ -15,6 +15,6 @@ const LIMIT_LISTS = [
 
 const LIMIT_DEFAULT = LIMIT_LISTS[0].value;
 const PAGE_DEFAULT = 1;
-const DELAY = 500;
+const DELAY = 1000;
 
 export { DELAY, LIMIT_LISTS, LIMIT_DEFAULT, PAGE_DEFAULT };
