d7ad1cd6b26bc27064cd3111fca7d5226bfcbeae
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-09-24 11:56:29 +0700

    feat(client): add DeleteEventModal component and hook

---
+++ client/src/components/views/Admin/Event/DeleteEventModal/useDeleteEventModal.tsx
@@ -0,0 +1,43 @@
+import { ToasterContext } from "@/contexts/ToasterContext";
+import eventServices from "@/services/event.service";
+import { useMutation } from "@tanstack/react-query";
+import { useContext } from "react";
+
+const useDeleteEventModal = () => {
+    const { setToaster } = useContext(ToasterContext);
+
+    const deleteEvent = async (id: string) => {
+        const res = await eventServices.deleteEvent(id);
+
+        return res;
+    };
+
+    const {
+        mutate: mutateDeleteEvent,
+        isPending: isPendingMutateDeleteEvent,
+        isSuccess: isSuccessMutateDeleteEvent,
+    } = useMutation({
+        mutationFn: deleteEvent,
+        onSuccess: () => {
+            setToaster({
+                type: "success",
+                message: "Event deleted successfully",
+            });
+        },
+        onError: (error) => {
+            setToaster({
+                type: "error",
+                message: error.message,
+            });
+        },
+    });
+
+
+    return {
+        mutateDeleteEvent,
+        isPendingMutateDeleteEvent,
+        isSuccessMutateDeleteEvent,
+    };
+};
+
+export default useDeleteEventModal;
